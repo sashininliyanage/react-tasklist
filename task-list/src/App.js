f2e3686863c56cafd6c3036f94c8946af0676f69
“sashininliyanage” <nimthara.liyanage@gmail.com>
2021-11-28 14:12:21 +0530

    add about page

--- task-list/src/App.js
+++ task-list/src/App.js
@@ -1,7 +1,10 @@
 import {useState, useEffect} from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import About from "./components/About";
 
 const App = ()=> {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -62,24 +65,34 @@ const toggleReminder = async(id)=>{
 
   const res = await fetch(`http://localhost:5000/tasks/${id}`,
   {method: 'PUT',
-   headers: {'Content-Type': 'application/json'},
-   body: JSON.stringify(upTask)
+  headers: {'Content-Type': 'application/json'},
+  body: JSON.stringify(upTask)
   })
 
   const data = await res.json()
 
-  setTasks(tasks.map((task)=> task.id === id? {...task,reminder:!task.reminder}:task))
+  setTasks(tasks.map((task)=> task.id === id? {...task,reminder:data.reminder}:task))
 }
 
   return ( 
-  <div className = "container" >
+    <Router>
+    <div className = "container" >
     <Header onAdd={()=> setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-    {showAddTask &&<AddTask onAdd={addTask}/>}
-    {tasks.length>0 ?
-    (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>):('No tasks to show')
-    }
-    
+    <Routes>
+
+    <Route path='/' element={
+      <>
+      {showAddTask &&<AddTask onAdd={addTask}/>}
+      {tasks.length>0 ?
+      (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>):('No tasks to show')
+      }
+      </>
+    }/>
+    <Route path='/about' element={<About/>}/>
+    </Routes>
+    <Footer/>
   </div>
+  </Router>
   );
 }
 
